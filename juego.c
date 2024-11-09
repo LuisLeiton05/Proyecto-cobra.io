55742be2a3e9a0df18c94616cf6e73d872e8c280
luisdrl <ramirezluisleiton@gmail.com>
2024-11-09 15:54:00 -0600

    Se crea el arhivo y se dividen las tareas en 3 partes fundamentales.

---
+++ juego.c
@@ -0,0 +1,19 @@
+/*
+    Logica del juego: movimiento de la serpiente , choques, crecimiento, comida, etc.
+*/ 
+
+
+
+/*
+    Interfaz del juego: visualizacion de la serpiente (forma), comida, tablero, etc.
+*/ 
+
+
+
+
+/*
+    Menu del juego: apariencia, colores, boton de jugar, mejor tiempo, etc.
+*/ 
+
+
+
