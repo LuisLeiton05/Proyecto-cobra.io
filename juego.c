2ce649a2c62772effd5147ae669eadc047cda671
luisdrl <ramirezluisleiton@gmail.com>
2024-11-14 21:36:06 -0600

    Se crea la función y lógica para inicializar SDL y verificar si se inicializa.

--- juego.c
+++ juego.c
@@ -1,3 +1,9 @@
+#include <SDL3/SDL.h>
+
+#include <stdio.h> 
+#include <stdbool.h>
+
+
 /*
     Logica del juego: movimiento de la serpiente , choques, crecimiento, comida, etc.
 */ 
@@ -8,6 +14,38 @@
     Interfaz del juego: visualizacion de la serpiente (forma), comida, tablero, etc.
 */ 
 
+//Definimos los parámetros a usar en las funciones:
+const int ancho_ventana =  640 ;
+const int altura_ventana = 480 ;
+const int tamaño_celdas = 20 ;
+
+// Función para inicializar.
+bool inicializar_SDL () {
+    if ( SDL_Init(SDL_INIT_VIDEO) != 0 ) {
+        printf ("Error al inicializar SDL: %s \n ", SDL_GetError()) ;
+        return 1 ; 
+    }
+
+    return 0;
+}
+
+int main () {
+    // Inicializamos.
+     if ( inicializar_SDL != 0 ) {
+        printf ("Error al inicializar SDL: %s \n ", SDL_GetError()) ;
+        return 0 ; // Sale si lo cumple.
+    } 
+    
+
+
+
+
+// Finalizar el programa.
+
+SDL_Quit() ;
+
+
+}
 
 
 
