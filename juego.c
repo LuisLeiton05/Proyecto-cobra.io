c47f7f9ee7129ba5f5ad52469ebe071af86cda43
luisdrl <ramirezluisleiton@gmail.com>
2024-11-15 01:53:32 -0600

    Se agrega la función para dibujar la comida.

--- juego.c
+++ juego.c
@@ -5,6 +5,7 @@
 
 
 
+
 /*
     Interfaz del juego: visualizacion de la serpiente (forma), comida, tablero, etc.
 */ 
@@ -33,7 +34,7 @@ bool inicializar_SDL () {
 // Función para crear una ventana.
 bool crear_ventana (SDL_Window ** ventana) {
     * ventana = SDL_CreateWindow("cobra.io", ancho_ventana, altura_ventana, SDL_WINDOW_MINIMIZED) ;
-    if (ventana == NULL ) {
+    if ( * ventana == NULL ) {
         printf ("Error al crear la ventana: %s \n ", SDL_GetError()) ;
         SDL_Quit () ;
         return false ; 
@@ -42,8 +43,8 @@ bool crear_ventana (SDL_Window ** ventana) {
 }
 
 // Función para crear el renderizador.
-bool crear_renderizador ( SDL_Renderer ** renderizador,SDL_Window ** ventana ) {
-    SDL_Renderer *renderizador = SDL_CreateRenderer(ventana, NULL) ;
+bool crear_renderizador ( SDL_Renderer ** renderizador,SDL_Window * ventana ) {
+    * renderizador = SDL_CreateRenderer(ventana, NULL) ;
     if (renderizador == NULL ) {
         printf ("Error al crear el renderizador: %s \n ", SDL_GetError()) ;
         SDL_DestroyWindow (ventana) ;
@@ -54,7 +55,7 @@ bool crear_renderizador ( SDL_Renderer ** renderizador,SDL_Window ** ventana ) {
 }
 
 // Función para dibujar las celdas.
-void dibujar_tablero (SDL_Renderer ** renderizador){
+void dibujar_tablero (SDL_Renderer * renderizador){
     for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
                 SDL_Rect celda = { j * tamaño_celdas, i * tamaño_celdas, tamaño_celdas, tamaño_celdas };
@@ -62,17 +63,31 @@ void dibujar_tablero (SDL_Renderer ** renderizador){
                 SDL_RenderFillRect(renderizador, &celda);
             }
         }
+}
 
+// Función para dibujar la serpiente y localización.
+void dibujar_serpiente (int size, SDL_Point *tamano_serpiente, SDL_Renderer * renderizador ) {
+    for (int i = 0; i < size; i++) {
+        SDL_Rect rect = { tamano_serpiente[i].x * tamaño_celdas, tamano_serpiente[i].y * tamaño_celdas, tamaño_celdas, tamaño_celdas };
+        SDL_SetRenderDrawColor(renderizador, 0, 255, 0, 255); // Color verde para la serpiente
+        SDL_RenderFillRect(renderizador, &rect);
+    }
 
 }
 
-// Dibujar la serpiente y localización.
-void dibujar_serpiente (int size) {
+// Función para dibujar la comida y su localización.
+void dibujar_comida (SDL_Renderer * renderizador) {
+    // Definir la posición de la comida.
+    SDL_Point comida ;
+    comida.x = rand() % columnas;
+    comida.y = rand() % filas;
 
+    SDL_Rect rect = { comida.x * tamaño_celdas, comida.y * tamaño_celdas, tamaño_celdas, tamaño_celdas };
+    SDL_SetRenderDrawColor(renderizador, 255, 0, 0, 255); // Color rojo para la comida
+    SDL_RenderFillRect(renderizador, &rect);
 
 }
 
-// Dibujar la comida y localización.
 
 int main () {
     // Inicializamos.
@@ -90,29 +105,36 @@ int main () {
     
     // LLamamos la función del renderizador.
     SDL_Renderer * renderizador2 ;
-    if ( crear_renderizador(&renderizador2, &ventana2) != true) {
+    if ( crear_renderizador(&renderizador2, ventana2) != true) {
         printf ("Error al crear la ventana: %s \n ", SDL_GetError()) ;
         return 0 ; // Sale si lo cumple.
     } 
 
         // Dibujamos las celdas/tablero.
         dibujar_tablero(&renderizador2) ;
+
         // Dibujamos la serpiente y su localización.
         int size = 1;
-        int *tamano_serpiente = (int *)malloc(size * sizeof(int *)) ;
+        SDL_Point *tamano_serpiente = (SDL_Point *)malloc(size * sizeof(SDL_Point)) ;
+        tamano_serpiente[0].x = 5 ;
+        tamano_serpiente[0].y = 5 ; // Posición inicial-
+        dibujar_serpiente(size, tamano_serpiente, renderizador2);
 
         //Dibujamos la comida y su localización.
+        dibujar_comida(renderizador2) ;
+
+        // Mostramos todo junto.
+        SDL_RenderPresent(renderizador2) ;
 
 
-    // 
+    // Bucle del juego.
 
 
     // Finalizar el programa y liberar recursos.
+    free (tamano_serpiente) ;
     SDL_DestroyRenderer (renderizador2) ;
     SDL_DestroyWindow (ventana2);
     SDL_Quit() ;
-
-
 
     return 0;
 }
@@ -122,6 +144,3 @@ int main () {
 /*
     Menu del juego: apariencia, colores, boton de jugar, mejor tiempo, etc.
 */ 
-
-
-
